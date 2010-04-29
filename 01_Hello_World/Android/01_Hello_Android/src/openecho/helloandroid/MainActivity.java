1109d8d824f618144cb5c71c1bf105abc85121e1
openecho <j.w.marsden@gmail.com>
2010-04-29 13:47:06 +1000

    Upload Challenge 01 - Android

---
+++ 01_Hello_World/Android/01_Hello_Android/src/openecho/helloandroid/MainActivity.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package openecho.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Android Implementation for challenge 01
+ *
+ * @author openecho
+ */
+public class MainActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+    }
+
+}
