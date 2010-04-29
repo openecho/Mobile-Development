110e688a788f54f129d41de3dee02f98be6a6704
openecho <j.w.marsden@gmail.com>
2010-04-29 16:03:15 +1000

    Update to icon and layout

--- 01_Hello_World/Android/01_Hello_Android/src/openecho/helloandroid/R.java
+++ 01_Hello_World/Android/01_Hello_Android/src/openecho/helloandroid/R.java
@@ -10,11 +10,14 @@ package openecho.helloandroid;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int icon_01=0x7f020000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
-        public static final int greeting=0x7f030001;
+        public static final int app_name=0x7f040000;
+        public static final int greeting=0x7f040001;
     }
 }
