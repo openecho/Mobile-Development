f4685b8873b1488965216c39d8c9ef42326c229e
Predominant <predominant@Predominants-MacBook-Pro.local>
2010-05-01 20:37:56 +1000

    Added iPhone/iPad universal for 02 Hello Image.

---
+++ iPhone/02_Hello_Image/Shared/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  02_Hello_Image
+//
+//  Created by Predominant on 1/05/10.
+//  Copyright Graham Weldon 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
---
+++ iPhone/02_Hello_Image/iPad/AppDelegate_iPad.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate_iPad.m
+//  02_Hello_Image
+//
+//  Created by Predominant on 1/05/10.
+//  Copyright Graham Weldon 2010. All rights reserved.
+//
+
+#import "AppDelegate_iPad.h"
+
+@implementation AppDelegate_iPad
+
+@synthesize window;
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+	
+    // Override point for customization after application launch
+	
+    [window makeKeyAndVisible];
+	
+	return YES;
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
